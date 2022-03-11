6ad2d63910649a9eb1a23226f2d09ecd676da8bd
Corban <sonnemanc@gmail.com>
2022-03-11 12:38:30 -0800

    Done with assignment

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,22 @@
 // your Bomb code here!
+import React, { Component } from 'react';
+
+class Bomb extends React.Component {
+  constructor(props) {
+      super();
+      this.state = {
+        secondsLeft: props.initialCount
+      }
+  }
+
+  timeOut = () => {
+      if (this.state.secondsLeft === 0) 
+        {return `Boom!`} else {return this.state.secondsLeft + ` seconds left before I go boom!`}
+  }
+
+  render() {
+      return <div>{this.timeOut()}</div>
+  }
+}
+
+export default Bomb
